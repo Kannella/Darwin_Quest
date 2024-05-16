3d5faac91588fadf778382cb0a253b33e02d748b
Giovanni Canella <gi.canella84@gmail.com>
2024-05-16 18:04:39 -0300

    tela de creditos e de consquistas pronta

--- src/main/java/br/maua/teste/TelaInicial.java
+++ src/main/java/br/maua/teste/TelaInicial.java
@@ -110,47 +110,26 @@ public class TelaInicial extends javax.swing.JFrame {
     }//GEN-LAST:event_iniciarMouseClicked
 
     private void conquistasMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_conquistasMouseClicked
-        System.out.println("clicou conquistas");
+        dispose();
+        TelaConquistas telaConquistas = new TelaConquistas();
+        telaConquistas.setSize(768, 615);  // Defina o tamanho desejado da janela aqui
+        telaConquistas.setVisible(true);
+        telaConquistas.setLocationRelativeTo(null);
     }//GEN-LAST:event_conquistasMouseClicked
 
     private void creditosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_creditosMouseClicked
-        System.out.println("clicou creditos");
+        dispose();
+        TelaCreditos telaCreditos = new TelaCreditos();
+        telaCreditos.setSize(807, 563);  // Defina o tamanho desejado da janela aqui
+        telaCreditos.setVisible(true);
+        telaCreditos.setLocationRelativeTo(null);
+
     }//GEN-LAST:event_creditosMouseClicked
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-        */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TelaInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TelaInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TelaInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TelaInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new TelaInicial().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel conquistas;
