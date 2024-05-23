cda728a0e54429d43fb577860f7c9dbef46438bc
Giovanni Canella <gi.canella84@gmail.com>
2024-05-23 20:29:35 -0300

    Todas as telas criadas

---
+++ src/main/java/br/maua/teste/DialogoExplicacao2.java
@@ -0,0 +1,206 @@
+package br.maua.teste;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import javax.swing.plaf.basic.BasicScrollBarUI;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DialogoExplicacao2 extends javax.swing.JFrame {
+
+    private String[] textos = {
+            "Quando criei minha teoria, o nome “seleção natural” me pareceu bem apropriado",
+            "Afinal, é quase como se o ambiente da natureza que os animais estão inseridos, selecionassem aqueles que irão prosperar!"
+    };
+    private int index = 0;
+    private int textIndex = 0;
+    private Timer timer;
+
+    private JTextArea[] textAreas;
+
+    public DialogoExplicacao2() {
+        initComponents();
+        iniciarAnimacaoTexto();
+    }
+
+    @SuppressWarnings("unchecked")
+    private void initComponents() {
+
+        fala_balao = new javax.swing.JScrollPane();
+        jTextArea2 = new javax.swing.JTextArea();
+        fala_balao1 = new javax.swing.JScrollPane();
+        jTextArea3 = new javax.swing.JTextArea();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        darwin = new javax.swing.JLabel();
+        primeiro_dialogo = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(768, 576));
+        getContentPane().setLayout(null);
+
+        textAreas = new JTextArea[]{jTextArea3, jTextArea2}; // Primeiro o balão de cima, depois o de baixo
+        configurarTextArea(jTextArea2);
+        configurarTextArea(jTextArea3);
+
+        configurarScrollPane(fala_balao, jTextArea2);
+        configurarScrollPane(fala_balao1, jTextArea3);
+
+        getContentPane().add(fala_balao);
+        getContentPane().add(fala_balao1);
+
+        fala_balao.setBounds(300, 340, 420, 80);
+        fala_balao1.setBounds(90, 120, 420, 80);
+
+        jLabel3.setFont(new java.awt.Font("Arial", 0, 13));
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/caixa_dialogo.png")));
+        jLabel3.setToolTipText("");
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(260, 310, 490, 140);
+
+        jLabel1.setFont(new java.awt.Font("Arial", 0, 13));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/caixa_dialogo.png")));
+        jLabel1.setToolTipText("");
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(50, 90, 490, 140);
+
+        darwin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/darwinframe.png")));
+        getContentPane().add(darwin);
+        darwin.setBounds(20, 310, 210, 220);
+
+        primeiro_dialogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/tela_dialogo_resized.png")));
+        primeiro_dialogo.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        primeiro_dialogo.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                primeiro_dialogoMouseClicked(evt);
+            }
+        });
+        getContentPane().add(primeiro_dialogo);
+        primeiro_dialogo.setBounds(0, 0, 768, 576);
+
+        pack();
+    }
+
+    private void configurarTextArea(JTextArea textArea) {
+        textArea.setColumns(20);
+        textArea.setRows(5);
+        textArea.setBorder(new LineBorder(Color.WHITE, 1));
+        textArea.setFont(new Font("Arial", Font.PLAIN, 18));
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setEditable(false);
+    }
+
+    private void configurarScrollPane(JScrollPane scrollPane, JTextArea textArea) {
+        scrollPane.setViewportView(textArea);
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setBorder(null);
+
+        JScrollBar verticalScrollBar = scrollPane.getVerticalScrollBar();
+        verticalScrollBar.setPreferredSize(new Dimension(10, verticalScrollBar.getHeight()));
+        verticalScrollBar.setUI(new BasicScrollBarUI() {
+            @Override
+            protected JButton createDecreaseButton(int orientation) {
+                return createZeroButton();
+            }
+
+            @Override
+            protected JButton createIncreaseButton(int orientation) {
+                return createZeroButton();
+            }
+
+            private JButton createZeroButton() {
+                JButton button = new JButton();
+                Dimension zeroDim = new Dimension(0, 0);
+                button.setPreferredSize(zeroDim);
+                button.setMinimumSize(zeroDim);
+                button.setMaximumSize(zeroDim);
+                return button;
+            }
+
+            @Override
+            protected void paintTrack(Graphics g, JComponent c, Rectangle trackBounds) {
+                // No track painting
+            }
+
+            @Override
+            protected void paintThumb(Graphics g, JComponent c, Rectangle thumbBounds) {
+                Graphics2D g2 = (Graphics2D) g.create();
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                Color color;
+                JScrollBar sb = (JScrollBar) c;
+                if (!sb.isEnabled() || thumbBounds.height > thumbBounds.width) {
+                    return;
+                } else if (isDragging) {
+                    color = new Color(0, 153, 255);
+                } else if (isThumbRollover()) {
+                    color = new Color(102, 102, 102);
+                } else {
+                    color = new Color(192, 192, 192);
+                }
+                g2.setPaint(color);
+                g2.fillRoundRect(thumbBounds.x, thumbBounds.y, thumbBounds.width, thumbBounds.height, 10, 10);
+                g2.setPaint(Color.BLACK);
+                g2.drawRoundRect(thumbBounds.x, thumbBounds.y, thumbBounds.width, thumbBounds.height, 10, 10);
+                g2.dispose();
+            }
+        });
+    }
+
+    private void iniciarAnimacaoTexto() {
+        timer = new Timer(50, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (index < textos[textIndex].length()) {
+                    textAreas[textIndex].append(String.valueOf(textos[textIndex].charAt(index)));
+                    index++;
+                } else {
+                    timer.stop();
+                    if (textIndex < textos.length - 1) {
+                        textIndex++;
+                        index = 0;
+                        timer.start();
+                    }
+                }
+            }
+        });
+        timer.start();
+    }
+
+    private void primeiro_dialogoMouseClicked(java.awt.event.MouseEvent evt) {
+        // TODO add your handling code here
+    }
+
+    public static void main(String args[]) {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(DialogoExplicacao2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        java.awt.EventQueue.invokeLater(() -> {
+            DialogoExplicacao2 dialog = new DialogoExplicacao2();
+            dialog.setVisible(true);
+        });
+    }
+
+    // Variables declaration - do not modify                     
+    private javax.swing.JLabel darwin;
+    private javax.swing.JScrollPane fala_balao;
+    private javax.swing.JScrollPane fala_balao1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JTextArea jTextArea2;
+    private javax.swing.JTextArea jTextArea3;
+    private javax.swing.JLabel primeiro_dialogo;
+    // End of variables declaration                   
+}
---
+++ src/main/java/br/maua/teste/PerpetuouBesouro.java
@@ -0,0 +1,186 @@
+package br.maua.teste;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import javax.swing.plaf.basic.BasicScrollBarUI;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PerpetuouBesouro extends javax.swing.JFrame {
+    
+    private String[] textos = {
+            "PARABÉNS CARO ALUNO, VOCÊ CONSEGUIU PERPETUAR A ESPÉCIE DE BESOUROS!!",
+            "AGORA SUA PROLE ESTÁ MAIS EVOLUÍDA E APTA AO AMBIENTE AO SEU REDOR!"
+    };
+    private int index = 0;
+    private int textIndex = 0;
+    private Timer timer;
+    
+    private JTextArea[] textAreas;
+
+    public PerpetuouBesouro() {
+        initComponents();
+        iniciarAnimacaoTexto();
+    }
+
+    @SuppressWarnings("unchecked")
+    private void initComponents() {
+
+        fala_balao_2 = new javax.swing.JScrollPane();
+        jTextArea2 = new javax.swing.JTextArea();
+        fala_balao_1 = new javax.swing.JScrollPane();
+        jTextArea3 = new javax.swing.JTextArea();
+        balaozao1 = new javax.swing.JLabel();
+        balaozao2 = new javax.swing.JLabel();
+        darwin = new javax.swing.JLabel();
+        fundo = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(768, 576));
+        setSize(new java.awt.Dimension(768, 576));
+        getContentPane().setLayout(null);
+
+        textAreas = new JTextArea[]{jTextArea2, jTextArea3};
+        configurarTextArea(jTextArea2);
+        configurarTextArea(jTextArea3);
+
+        configurarScrollPane(fala_balao_2, jTextArea2);
+        configurarScrollPane(fala_balao_1, jTextArea3);
+
+        getContentPane().add(fala_balao_2);
+        fala_balao_2.setBounds(100, 80, 280, 100);
+
+        getContentPane().add(fala_balao_1);
+        fala_balao_1.setBounds(350, 310, 280, 100);
+
+        balaozao1.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
+        balaozao1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        balaozao1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/balao_grande.png"))); // NOI18N
+        balaozao1.setToolTipText("");
+        getContentPane().add(balaozao1);
+        balaozao1.setBounds(60, 40, 360, 180);
+
+        balaozao2.setFont(new java.awt.Font("Arial", 0, 13)); // NOI18N
+        balaozao2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        balaozao2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/balao_grande.png"))); // NOI18N
+        balaozao2.setToolTipText("");
+        getContentPane().add(balaozao2);
+        balaozao2.setBounds(310, 260, 360, 200);
+
+        darwin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/darwinframe.png"))); // NOI18N
+        getContentPane().add(darwin);
+        darwin.setBounds(20, 310, 210, 220);
+
+        fundo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/fundo_pos_besouro.png"))); // NOI18N
+        getContentPane().add(fundo);
+        fundo.setBounds(0, 0, 770, 560);
+
+        pack();
+    }
+
+    private void configurarTextArea(JTextArea textArea) {
+        textArea.setColumns(20);
+        textArea.setRows(5);
+        textArea.setBorder(new LineBorder(Color.WHITE, 1));
+        textArea.setFont(new Font("Arial", Font.PLAIN, 18));
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        textArea.setEditable(false);
+    }
+
+    private void configurarScrollPane(JScrollPane scrollPane, JTextArea textArea) {
+        scrollPane.setViewportView(textArea);
+        scrollPane.setBounds(50, 40, 400, 80);
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setBorder(null);
+
+        JScrollBar verticalScrollBar = scrollPane.getVerticalScrollBar();
+        verticalScrollBar.setPreferredSize(new Dimension(10, verticalScrollBar.getHeight()));
+        verticalScrollBar.setUI(new BasicScrollBarUI() {
+            @Override
+            protected JButton createDecreaseButton(int orientation) {
+                return createZeroButton();
+            }
+
+            @Override
+            protected JButton createIncreaseButton(int orientation) {
+                return createZeroButton();
+            }
+
+            private JButton createZeroButton() {
+                JButton button = new JButton();
+                Dimension zeroDim = new Dimension(0, 0);
+                button.setPreferredSize(zeroDim);
+                button.setMinimumSize(zeroDim);
+                button.setMaximumSize(zeroDim);
+                return button;
+            }
+
+            @Override
+            protected void paintTrack(Graphics g, JComponent c, Rectangle trackBounds) {
+                // No track painting
+            }
+
+            @Override
+            protected void paintThumb(Graphics g, JComponent c, Rectangle thumbBounds) {
+                Graphics2D g2 = (Graphics2D) g.create();
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                Color color;
+                JScrollBar sb = (JScrollBar) c;
+                if (!sb.isEnabled() || thumbBounds.height > thumbBounds.width) {
+                    return;
+                } else if (isDragging) {
+                    color = new Color(0, 153, 255);
+                } else if (isThumbRollover()) {
+                    color = new Color(102, 102, 102);
+                } else {
+                    color = new Color(192, 192, 192);
+                }
+                g2.setPaint(color);
+                g2.fillRoundRect(thumbBounds.x, thumbBounds.y, thumbBounds.width, thumbBounds.height, 10, 10);
+                g2.setPaint(Color.BLACK);
+                g2.drawRoundRect(thumbBounds.x, thumbBounds.y, thumbBounds.width, thumbBounds.height, 10, 10);
+                g2.dispose();
+            }
+        });
+    }
+
+    private void iniciarAnimacaoTexto() {
+        timer = new Timer(50, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (index < textos[textIndex].length()) {
+                    textAreas[textIndex].append(String.valueOf(textos[textIndex].charAt(index)));
+                    index++;
+                } else {
+                    timer.stop();
+                    if (textIndex < textos.length - 1) {
+                        textIndex++;
+                        index = 0;
+                        timer.start();
+                    }
+                }
+            }
+        });
+        timer.start();
+    }
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new PerpetuouBesouro().setVisible(true);
+        });
+    }
+
+    // Variables declaration - do not modify                     
+    private javax.swing.JLabel balaozao1;
+    private javax.swing.JLabel balaozao2;
+    private javax.swing.JLabel darwin;
+    private javax.swing.JScrollPane fala_balao_1;
+    private javax.swing.JScrollPane fala_balao_2;
+    private javax.swing.JLabel fundo;
+    private javax.swing.JTextArea jTextArea2;
+    private javax.swing.JTextArea jTextArea3;
+    // End of variables declaration                   
+}
